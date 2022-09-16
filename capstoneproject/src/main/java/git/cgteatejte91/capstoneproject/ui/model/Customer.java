b6d69e553c03eb4b243f2bb8fb78f84500caa568
Calvin Teater <laughingglint3@gmail.com>
2022-09-16 15:50:10 -0500

    add additonal superclass fields

--- capstoneproject/src/main/java/git/cgteatejte91/capstoneproject/ui/model/Customer.java
+++ capstoneproject/src/main/java/git/cgteatejte91/capstoneproject/ui/model/Customer.java
@@ -1,5 +1,6 @@
 package git.cgteatejte91.capstoneproject.ui.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,9 +8,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class Customer extends WebsiteUser{
 
-    public Customer(String firstName, String lastName, String username, String email, String phoneNumber, Gender gender,
-            Address address, LocalDateTime created) {
-        super(firstName, lastName, username, email, phoneNumber, gender, address, created);
+    public Customer(String firstName, String lastName, String username, String email, String password, LocalDate dob,
+            Integer age, String phoneNumber, Gender gender, Address address, LocalDateTime created) {
+        super(firstName, lastName, username, email, password, dob, age, phoneNumber, gender, address, created);
     }
-    
+
+
 }
