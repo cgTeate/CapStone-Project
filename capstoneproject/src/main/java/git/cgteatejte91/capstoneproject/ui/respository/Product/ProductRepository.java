f827179bb4cde7e2e16b0666143311a0cc828a57
Calvin Teater <laughingglint3@gmail.com>
2022-09-21 20:30:17 -0500

    created product and shoe repository

---
+++ capstoneproject/src/main/java/git/cgteatejte91/capstoneproject/ui/respository/Product/ProductRepository.java
@@ -0,0 +1,13 @@
+package git.cgteatejte91.capstoneproject.ui.respository.Product;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import git.cgteatejte91.capstoneproject.ui.model.Product.Product;
+
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String>{
+    
+}
+
---
+++ capstoneproject/src/main/java/git/cgteatejte91/capstoneproject/ui/respository/Product/ShoeRepository.java
@@ -0,0 +1,13 @@
+package git.cgteatejte91.capstoneproject.ui.respository.Product;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import git.cgteatejte91.capstoneproject.ui.model.Product.Shoe;
+
+
+@Repository
+public interface ShoeRepository extends MongoRepository<Shoe, String>{
+    
+}
+
