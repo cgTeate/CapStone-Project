5a66fb6d0934ac82cef448d5e17f83893ae4655b
jte91 <j3sh0n3dward5@gmail.com>
2022-10-31 08:37:40 -0500

    shipping page created

--- capstoneproject/src/frontend/pages/_app.js
+++ capstoneproject/src/frontend/pages/_app.js
@@ -4,17 +4,36 @@ import {theme} from '../styles/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 import { StoreProvider } from '../utils/Store'
 import { SessionProvider, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
     <StoreProvider><ChakraProvider theme={theme}>
-      
+    {Component.auth ? (
+            <Auth adminOnly={Component.auth.adminOnly}>
         <Component {...pageProps} />
-        
+        </Auth>
+        ) : (
+          <Component {...pageProps} />
+          )}
     </ChakraProvider></StoreProvider>
   </SessionProvider>
 ) }
 
+function Auth ({ children }) {
+  const router = useRouter();
+  const { status} = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/unauthorized?message=login required');
+    },
+  });
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
 
-export default MyApp
+  return children;
+}
+
+export default MyApp;
